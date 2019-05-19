76f5042309bee59a763ffbf0a451d89a22e9450f
Maryam Kamel <maryam.kamel@linkdev.com>
2019-05-19 13:58:42 +0200

    adding crossfade effect

---
+++ Assets/Crossfadeeffect/Scripting/CrossFadePanel.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine.Events;
+using UnityEngine;
+/// <summary>
+/// this class is responsible for the fading effect on the camera when going up or doen
+/// </summary>
+public class CrossFadePanel : MonoBehaviour
+{
+    //event connected with the fade animation eventso that we switch position upon low opacity
+    public static UnityEvent SwitchPosition = new UnityEvent();
+    public static GameObject BlackImage;
+    private static Animator CrossFadeAnim;//Animator fading effect
+	// Use this for initialization
+	void Start ()
+    {
+        CrossFadeAnim = GetComponent<Animator>();
+        BlackImage = transform.GetChild(0).gameObject;
+        BlackImage.SetActive(false);
+	}
+    /// <summary>
+    /// this function will be called from the animation event to invoke the switch position event
+    /// </summary>
+    public void FireAnimationEvent()
+    {
+        SwitchPosition.Invoke();
+    }
+    //function that triggers the animation when going up or down
+    public static void PlayCrossFadeAnim()
+    {
+        BlackImage.SetActive(true);
+        CrossFadeAnim.SetTrigger("play");
+    }
+}
---
+++ Assets/Crossfadeeffect/Scripting/UPDownToggle.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+public class UPDownToggle :InteractiveObject
+{
+    [Tooltip("the target transform")]
+    public Transform Target;
+    private bool Subscribe = false; // subscribtion of animation event
+    protected override void ApplyAction()
+    {
+        if (!Subscribe)
+        {
+            CrossFadePanel.SwitchPosition.AddListener(SwitchtoTarget);
+            Subscribe = !Subscribe;
+        }
+        CrossFadePanel.PlayCrossFadeAnim();
+    }
+    /// <summary>
+    /// switches from the current transform to the target while animating
+    /// </summary>
+    private void SwitchtoTarget()
+    {
+        Camera.main.transform.parent.position = Target.position;
+        Camera.main.transform.rotation = Target.rotation;
+        Camera.main.fieldOfView = 60;
+        if (Subscribe)
+        {
+            CrossFadePanel.SwitchPosition.RemoveListener(SwitchtoTarget);
+            Subscribe = !Subscribe;
+        }
+    }
+}
