5d1a2002e1cffe58969ea7c1e763e7cee9be22c9
Maryam Kamel <maryam.kamel@linkdev.com>
2019-07-30 17:03:52 +0200

    assing holdhandler to swap two gameobjects

---
+++ Assets/HoldandSwap/Scripts/Holdhandler.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class Holdhandler : MonoBehaviour,IPointerDownHandler,IPointerUpHandler
+{
+    public UnityAction<GameObject> GotSelected;
+    private SwapHandler swhandler;
+    private bool SwapMode = false;
+    private int holdtime = 1;//seconds
+    private float dwntime = 0;
+    private float uptime = 0;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+
+    }
+    private void Start()
+    {
+        if (swhandler == null)
+        {
+            swhandler = FindObjectOfType<SwapHandler>();
+            swhandler.ActivateSwapMode.AddListener(OnSwapActivated);
+            swhandler.DeActivateSwapMode.AddListener(OnSwapDeActivated);
+        }
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        dwntime = Time.time;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        uptime = Time.time;
+        var deltati = uptime - dwntime;
+        if (deltati < holdtime)
+        {
+            if (SwapMode)
+            {
+                GotSelected(gameObject);
+            }
+            else
+            {
+
+            }
+        }
+        else
+        {
+            Debug.Log("hold detected");
+            if(!SwapMode)
+            GotSelected(gameObject);
+        }
+        dwntime = 0;
+        uptime = 0;
+    }
+    private void OnSwapActivated()
+    {
+        SwapMode = true;
+    }
+    private void OnSwapDeActivated()
+    {
+        SwapMode = false;
+    }
+}
---
+++ Assets/HoldandSwap/Scripts/SwapHandler.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class SwapHandler : MonoBehaviour
+{
+    public UnityEvent ActivateSwapMode = new UnityEvent();
+    public UnityEvent DeActivateSwapMode = new UnityEvent();
+    private GameObject replacedObject = null;
+    private GameObject replacementObject = null;
+ 
+    private Holdhandler[] holdables;
+    // Start is called before the first frame update
+    void Start()
+    {
+        holdables = FindObjectsOfType<Holdhandler>();
+        for (int i = 0; i < holdables.Length; i++)
+        {
+            holdables[i].GotSelected = OnGotselected;
+        }
+    }
+    
+    private void OnGotselected(GameObject selected)
+    {
+        if (replacedObject == null)
+        {
+            ActivateSwapMode.Invoke();
+            replacedObject = selected;
+        }
+        else if (replacementObject == null)
+        {
+            replacementObject = selected;
+        }
+        if (replacedObject && replacementObject)
+        {
+            SwapObjects();
+            DeActivateSwapMode.Invoke();
+            InitObjects();
+        }
+    }
+    private void SwapObjects()
+    {
+        //loading temp
+        var temppos = replacedObject.transform.position;
+        var tempscale = replacedObject.transform.localScale;
+        //assiening replacement values to replaced
+        replacedObject.transform.position = replacementObject.transform.position;
+        replacedObject.transform.localScale = replacementObject.transform.localScale;
+        //assigning temp values to replacement
+        replacementObject.transform.position = temppos;
+        replacementObject.transform.localScale = tempscale;
+    }
+    private void InitObjects()
+    {
+        replacementObject = null;
+        replacedObject = null;
+
+    }
+}
